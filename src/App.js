a8479d2064100a9ec476e441ddbeb2dfe42464ec
Carolzita Dev <42356399+devcarolzita@users.noreply.github.com>
2023-11-26 19:31:35 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import CustomButton from './components/CustomButton';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <ListarTarefa />
+      <CustomButton text="Clique Aqui" onClick={handleClick} />
+
     </div>
   );
 }
